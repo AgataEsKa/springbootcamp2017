f814e56ddefd3c2d16fac5905cc335558281a548
Arkadiusz Cacko <cackoarek@gmail.com>
2017-10-29 10:50:42 +0100

    DI - javaConfig - wstrzykiwanie przez konstruktor

--- src/main/java/pl/kodolamacz/spring/Human.java
+++ src/main/java/pl/kodolamacz/spring/Human.java
@@ -2,6 +2,7 @@ package pl.kodolamacz.spring;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.util.Assert;
 
 /**
  * Created by acacko on 29.10.17
@@ -9,22 +10,26 @@ import org.springframework.stereotype.Component;
 @Component
 public class Human {
 
+    @Autowired
     private Leg leg;
 
-    public Human() {}
-
-    @Autowired
-    public Human(Leg leg) {
-        this.leg = leg;
+    public Human() {
+        System.out.println("Bezparametrowy");
     }
+
+//    public Human(Leg leg) {
+//        Assert.notNull(leg, "Leg is NULL!");
+//        this.leg = leg;
+//        System.out.println("Wstrzykuję nogę");
+//    }
 
     public Leg getLeg() {
         return leg;
     }
 
-    public void setLeg(Leg leg) {
-        this.leg = leg;
-    }
+//    public void setLeg(Leg leg) {
+//        this.leg = leg;
+//    }
 
     @Override
     public String toString() {
